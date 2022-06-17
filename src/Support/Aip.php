2d19fbc18c44625b6f331b3dced5206a801cebf4
小瓜皮 <i@ihao.cc>
2022-06-18 02:59:34 +0800

    update readme

--- src/Support/Aip.php
+++ src/Support/Aip.php
@@ -7,20 +7,10 @@ use Buxuhunao\LaravelBaiduAip\BaiduAip;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static Response getUuid(string $code)
- * @method static Response bind(string $uuid, string $name, ?string $remark = null)
- * @method static Response unbind(string $uuid)
- * @method static Response info(string $uuid)
- * @method static Response list(int $page)
- * @method static Response edit(string $uuid, string $name, ?string $remark = null)
- * @method static Response capability(string $uuid)
- * @method static Response create(array $params)
- * @method static Response cancel(string $uuid)
- * @method static Response process(string $uuid, string $printUuid)
- * @method static Response reboot(string $uuid)
- * @method static Response headClean(string $uuid)
- * @method static Response flush(string $uuid)
- * @method static Response nozzleCheck(string $uuid)
+ * @method static Response add(array $options)
+ * @method static Response search(array $options)
+ * @method static Response update(array $options)
+ * @method static Response remove(array $options)
  */
 class Aip extends Facade
 {
