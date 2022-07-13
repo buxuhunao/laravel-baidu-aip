93fa2fe6d56a2f67be55a605f2812fc04b2b33de
小瓜皮 <i@ihao.cc>
2022-07-13 18:01:30 +0800

    修改响应数据为数组

--- src/BaiduAip.php
+++ src/BaiduAip.php
@@ -2,6 +2,8 @@
 
 namespace Buxuhunao\LaravelBaiduAip;
 
+use Buxuhunao\LaravelBaiduAip\Exceptions\Exception;
+
 class BaiduAip extends Client
 {
     public function __construct($config)
@@ -9,31 +11,42 @@ class BaiduAip extends Client
         parent::__construct($config);
     }
 
-   public function add($options): Http\Response
+   public function add($options): array
    {
        $uri = '/rest/2.0/image-classify/v1/realtime_search/similar/add';
 
-       return $this->postWithToken($uri, $options);
+       return $this->toArray($this->postWithToken($uri, $options));
    }
 
-    public function search($options): Http\Response
+    public function search($options): array
     {
         $uri = '/rest/2.0/image-classify/v1/realtime_search/similar/search';
 
-        return $this->postWithToken($uri, $options);
+        return $this->toArray($this->postWithToken($uri, $options));
     }
 
-    public function update($options): Http\Response
+    public function update($options): array
     {
         $uri = '/rest/2.0/image-classify/v1/realtime_search/similar/update';
 
-        return $this->postWithToken($uri, $options);
+        return $this->toArray($this->postWithToken($uri, $options));
     }
 
-    public function remove($options): Http\Response
+    public function remove($options): array
     {
         $uri = '/rest/2.0/image-classify/v1/realtime_search/similar/delete';
 
-        return $this->postWithToken($uri, $options);
+        return $this->toArray($this->postWithToken($uri, $options));
+    }
+
+    protected function toArray($response)
+    {
+        $array = $response->toArray();
+
+        if (! empty($array['error_code'])) {
+            throw new Exception($array['error_msg'], 500);
+        }
+
+        return $array;
     }
 }
